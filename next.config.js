ccc89d70fe11f7b0e4e21616e5d70c496f74a603
Yeow Zi Qin <ziqinyeow@gmail.com>
2022-06-06 18:56:35 +0800

    update policy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   reactStrictMode: true,
+  async header() {
+    return [
+      {
+        source: "*",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config, {}) => {
     config.resolve.extensions.push(".ts", ".tsx");
     config.resolve.fallback = { fs: false };
